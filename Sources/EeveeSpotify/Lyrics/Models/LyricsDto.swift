61388c57ee4bb587234d09dafad02ccbaa6cabd8
Charles L <Charles_L@outlook.jp>
2025-11-17 16:05:46 +0800

    Update LyricsDto.swift

--- Sources/EeveeSpotify/Lyrics/Models/LyricsDto.swift
+++ Sources/EeveeSpotify/Lyrics/Models/LyricsDto.swift
@@ -10,7 +10,7 @@ struct LyricsDto {
         var lyricsData = LyricsData.with {
             $0.timeSynchronized = timeSynced
             $0.restriction = .unrestricted
-            $0.providedBy = "\(qqmusic) (CharlesL)"
+            $0.providedBy = "\(source) (CharlesL)"
         }
         
         let shouldRomanize = UserDefaults.lyricsOptions.romanization
