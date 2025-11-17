e4bc319d3e4892757fa7ffa62ac36d3a2674a6aa
Charles L <Charles_L@outlook.jp>
2025-11-17 16:02:37 +0800

    Update LrclibSong.swift

--- Sources/EeveeSpotify/Lyrics/Models/Lrclib/LrclibSong.swift
+++ Sources/EeveeSpotify/Lyrics/Models/Lrclib/LrclibSong.swift
@@ -4,5 +4,6 @@ struct LrclibSong: Decodable {
     var name: String
     var plainLyrics: String?
     var syncedLyrics: String?
+    var translatedLyrics: String?  // 添加翻译歌词字段
     var instrumental: Bool
 }
