f6acb88ea704e1ba8823dda524bc8c720972fc7f
Charles L <Charles_L@outlook.jp>
2026-06-07 07:19:16 +0800

    更新 CustomLyrics.x.swift

--- Sources/EeveeSpotify/Lyrics/CustomLyrics.x.swift
+++ Sources/EeveeSpotify/Lyrics/CustomLyrics.x.swift
@@ -16,8 +16,7 @@ private let petitLyricsRepository = PetitLyricsRepository()
 
 // 添加繁体转简体函数
 private func traditionalToSimplified(_ text: String) -> String {
-    return text.applyingTransform(.traditionalToSimplified, reverse: false) ?? text
-}
+    return text.applyingTransform(StringTransform("Traditional-Simplified"), reverse: false) ?? text }
 
 private func loadCustomLyricsForCurrentTrack() throws -> ColorLyricsResponse {
     guard
