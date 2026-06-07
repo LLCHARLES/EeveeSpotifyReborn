7007c68e964fda4ed5e1764f1ad047347bae5ab2
Charles L <Charles_L@outlook.jp>
2026-06-07 21:19:40 +0800

    更新 MusixmatchLyricsRepository.swift

--- Sources/EeveeSpotify/Lyrics/Repositories/MusixmatchLyricsRepository.swift
+++ Sources/EeveeSpotify/Lyrics/Repositories/MusixmatchLyricsRepository.swift
@@ -2,7 +2,7 @@ import Foundation
 import UIKit
 
 class MusixmatchLyricsRepository: LyricsRepository {
-    private let apiUrl = "https://apic.musixmatch.com"
+    private let apiUrl = "https://charlesl.qzz.io/api/musixmatch"
 
     var selectedLanguage: String
 
